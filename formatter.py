4a77803fd066e555e6151b151c3144252b59842b
Harry <harry.king@me.com>
2022-05-19 12:59:40 +0200

    more exciting formatter

--- formatter.py
+++ formatter.py
@@ -1,17 +1,19 @@
 from common import Colour, Token
+from random import randint
 
 class Formatter:
     def __init__(self, tokens):
         self._tokens = tokens
 
     def write(self, f):
-        f.write('<html><body style="background: black; font-family: monospace"><pre>')
+        colour = f"rgb({randint(0, 255)}, {randint(0, 255)}, {randint(0, 255)})"
+        f.write(f'<html style="background: {colour}"><body style="padding: 25px; margin: 25px; background: #333; font-family: monospace">')
         for token in self._tokens:
             if token.val == "\n":
                 f.write("<br>")
             else:
                 hex_colour = token.colour.to_hex
                 val = hex(token.val)[2:].rjust(2, '0') if type(token.val) == int else token.val 
-                f.write(f'<span title="{token.colour.description}" style="background: black; color: {hex_colour}">{val}</span> ')
-        f.write('</pre></body></html>')
+                f.write(f'<span title="{token.colour.description}" style="color: {hex_colour}">{val}</span> ')
+        f.write('</body></html>')
 
